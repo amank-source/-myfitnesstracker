9474f70a8009975b56dd878890d5baa00cdfe832
fakhan <amank9008@gmail.com>
2020-11-25 20:36:10 -0600

    deign change and activity

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import Header from './Header'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
@@ -6,9 +6,37 @@ import Login from './Login'
 import { getToken, clearToken } from '../api/index'
 import { hitAPI, auth } from '../api/index'
 import Activities from './Activities'
+import Home from './Home'
+import NewActivity from './NewActivity'
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(!!getToken())
+  const [activitiesList, setActivitiesList] = useState([])
+
+  function updateActivity(updatedAct) {
+    let index = activitiesList.findIndex((activity) => {
+      return activity.id === updatedAct.id
+    })
+    if (index > -1) {
+      let activityList = [...activitiesList]
+      activityList[index] = updatedAct
+      setActivitiesList(activityList)
+    }
+  }
+
+  const addActivity = (newActivity) => {
+    return setActivitiesList([newActivity, ...activitiesList])
+  }
+
+  useEffect(() => {
+    hitAPI('GET', '/activities')
+      .then((data) => {
+        console.log(data)
+        setActivitiesList(data)
+      })
+      .catch(console.error)
+  }, [isLoggedIn])
+
   return (
     <Router>
       <div className="app">
@@ -24,7 +52,15 @@ function App() {
               clearToken={clearToken}
             />
 
-            <Activities />
+            <Activities
+              isLoggedIn={isLoggedIn}
+              activitiesList={activitiesList}
+              setActivitiesList={setActivitiesList}
+              addActivity={addActivity}
+              updateActivity={updateActivity}
+            />
+
+            <NewActivity activitiesList={activitiesList} />
           </Route>
           <Route path="/">
             <Header
@@ -32,7 +68,7 @@ function App() {
               setIsLoggedIn={setIsLoggedIn}
               clearToken={clearToken}
             />
-            <Activities />
+            <Home />
           </Route>
         </Switch>
       </div>
